28ff0692f8403d0c5837440bffab183442a70443
R. Bramaditya <bramadityaw@duck.com>
2025-10-01 11:50:58 +0700

    separate lsp server configs to different file

---
+++ lua/lsp/servers.lua
@@ -0,0 +1,27 @@
+return {
+  -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+  --
+  -- Some languages (like typescript) have entire language plugins that can be useful:
+  --    https://github.com/pmizio/typescript-tools.nvim
+  --
+  -- But for many setups, the LSP (`ts_ls`) will work just fine
+  -- ts_ls = {},
+  --
+  gopls = {},
+  rust_analyzer = {},
+  emmet_language_server = {},
+  intelephense = {},
+
+  lua_ls = {
+    -- cmd = { ... },
+    -- filetypes = { ... },
+    -- capabilities = {},
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = 'Replace',
+        },
+      },
+    },
+  },
+}
